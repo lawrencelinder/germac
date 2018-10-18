3072be7d4338cefa39aaa27227a4b3361293b7a5
Lawrence Linder <lawrencelinder@gmail.com>
2018-10-18 16:56:24 -0300

    Adicionado arquivo de configurações.

---
+++ config.py
@@ -0,0 +1,21 @@
+#
+# Configuração do banco de dados
+# By Lawrence Linder - 18/10/2018
+# Versão 1.0a
+#
+
+geral = {
+  "Versão": "1.0",
+  "nome": "GerMac",
+  "Lisensa": "MIT",
+  "Desenvolvedor": "Lawrence Linder",
+  "Contato": "lawrencelinder@gmail.com",
+  "Site": "nenhum",
+}
+
+banco = {
+  "usuario": "germac",
+  "senha": "camreg",
+  "database": "germac",
+  "server": "10.20.108.69"
+}
